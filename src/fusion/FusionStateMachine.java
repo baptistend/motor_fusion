73b9127fd075fe57d7a37b12ee4300093f54d3f6
Tom ROMANO <98322838+Tom-Romano@users.noreply.github.com>
2025-01-20 14:33:24 +0100

    :art: Avancement

--- src/fusion/FusionStateMachine.java
+++ src/fusion/FusionStateMachine.java
@@ -5,7 +5,9 @@ import fr.dgac.ivy.IvyClient;
 import fr.dgac.ivy.IvyException;
 import fr.dgac.ivy.IvyMessageListener;
 
+import java.util.Date;
 import java.util.Timer;
+import java.util.function.BiConsumer;
 
 public class FusionStateMachine implements IvyMessageListener {
     private Ivy bus;
@@ -23,42 +25,129 @@ public class FusionStateMachine implements IvyMessageListener {
     public FusionStateMachine() throws IvyException {
         this.state = State.Idle;
         bus = new Ivy("Fusion", "Fusion Ready", null);
-        bus.start("127.255.255.255:2010");
+        bus.start("127.0.0.1:2010");
+        bus.sendToSelf(true);
 
 
         //bind to reco2D
         bus.bindMsg("^Geste:Forme nom=(.*)", (client, args) -> {
-            //dessinerCercle(args, "green");
-            switch(args[0]){
-                case "circle":
-                    dessinerCercle(10,10, "black");
+            switch (state) {
+                case Idle:
+                    switch(args[0]){
+                        case "circle":
+                            dessinerCercle(10,10, "black");
+                            break;
+                        case "rectangle":
+                            dessinerRectangle(10,10, "black");
+                            break;
+                        default:
+                            break;
+                    }
+                    updateState(State.Create);
                     break;
-                case "rectangle":
-                    dessinerRectangle(10,10, "black");
+                case Move:
+                    System.out.println("Impossible de créer une forme quand State=Move.");
                     break;
-
+                case Create:
+                    System.out.println("Impossible de créer une forme quand State=Create.");
+                    break;
+                case Color:
+                    System.out.println("Impossible de créer une forme quand State=Color.");
+                    break;
+                case Position:
+                    System.out.println("Impossible de créer une forme quand State=Position.");
+                    break;
+                case Selected:
+                    System.out.println("Impossible de créer une forme quand State=Selected.");
+                    break;
                 default:
+                    System.out.println("Geste ignoré dans l'état actuel.");
                     break;
             }
-            updateState(State.Create);
-
         });
 
         bus.bindMsg("^Geste:Deplacer", (client, args) -> {
-            //dessinerCercle(args, "green");
+            switch (state) {
+                case Idle:
+                    updateState(State.Move);
+                    break;
+                case Move:
+                    System.out.println("Déjà dans State=Move.");
+                    break;
+                case Create:
+                    System.out.println("Impossible de bouger quand State=Create.");
+                    break;
+                case Color:
+                    System.out.println("Impossible de bouger quand State=Color.");
+                    break;
+                case Position:
+                    System.out.println("Impossible de bouger quand State=Position.");
+                    break;
+                case Selected:
+                    System.out.println("Impossible de bouger quand State=Selected.");
+                    break;
+                default:
+                    System.out.println("Geste ignoré dans l'état actuel.");
+                    break;
+            }
         });
 
         //Reco vocale
         bus.bindMsg("^Geste:RecoVoc designation=(.*)", (client, args) -> {
-            //dessinerCercle(args, "green");
+            switch (state) {
+                case Idle:
+                    System.out.println("Rien a faire si reco-vocal quand State=Idle.");
+                    break;
+                case Move:
+                    //Faire un test sur la designation pour savoir si c'est "OBJET"
+                    break;
+                case Create:
+                    /* DEUX POSSIBILITES : Vers couleur ou vers position
+                    *   1. Ecouter la designation pour savoir si c'est "COULEUR" ou "POSITION"
+                    *   2. Si c'est "COULEUR", changer l'état en "Color"
+                    *   3. Si c'est "POSITION", changer l'état en "Position"
+                    * */
+                    break;
+                case Color:
+                    /* Regarder la position du curseur (sur objet)
+                    *  Écouter la designation
+                    *  Si c'est COULEUR changer la couleur de l'objet
+                    * */
+                    break;
+                case Position:
+                    /* Regarder la position du curseur (sur canva)
+                     *  Écouter la designation
+                     *  Si c'est POSITION, changer la position de l'objet
+                     * */
+                    break;
+                case Selected:
+                    /* Regarder la position du curseur (sur canva)
+                     *  Écouter la designation
+                     *  Si c'est POSITION, changer la position de l'objet
+                     *  Changer vers l'état Position
+                     * */
+                    break;
+                default:
+                    System.out.println("Geste ignoré dans l'état actuel.");
+                    break;
+            }
         });
 
         //Reco palette
-        bus.bindMsg("^Palette:ResultatTesterPoint x=(\\d+) y=(\\d+) nom=(.*)", (client, args) -> {
+        //S'envoie dans tous les cas après un test de point
+        bus.bindMsg("Palette:FinTesterPoint x=(\\d+) y=(\\d+)",  (client, args) -> {
+            System.out.println("FinTesterPoint x=" + args[0] + " y=" + args[1]);
+            //dessinerCercle(Integer.parseInt(args[0]), Integer.parseInt(args[1]), "green");
+        });
+
+        //S'envoie que si un objet est bien sous le curseur
+        bus.bindMsg("Palette:ResultatTesterPoint x=(\\d+) y=(\\d+) nom=(.*)", (client, args) -> {
+            System.out.println("ResultatTesterPoint x=" + args[0] + " y=" + args[1] + " nom=" + args[2]);
             //dessinerCercle(args, "green");
         });
 
     }
+
     private void updateState(State newState){
         switch (newState){
             case Idle:
@@ -98,5 +187,13 @@ public class FusionStateMachine implements IvyMessageListener {
     @Override
     public void receive(IvyClient client, String[] args) {
 
+    }
+
+    public static void main(String[] args) {
+        try {
+            new FusionStateMachine();
+        } catch (IvyException e) {
+            e.printStackTrace();
+        }
     }
 }
